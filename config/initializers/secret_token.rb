1fec431d348418c0f63141000d1975553dca3537
Jürg Schaeppi <info@schaeppi.tv>
2012-02-07 17:55:12 -0700

    first upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BdwNotes::Application.config.secret_token = 'd8d08765d774126fcfa74c535191ef8522c4fad6c27dc65e5773c1987372ae8ed17fec05f050eb6f1027f7d2ea289b4f740af24720526a85c05199f409246b75'
